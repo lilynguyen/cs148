26a530e60b43d604e39882ab3a16e413ec16e4ec
Lily Hoai Nguyen <lhnguyen@silk3.uvm.edu>
2015-11-06 13:41:43 -0500

    idk what edit to plan u did

--- advising/plan.php
+++ advising/plan.php
@@ -1,21 +1,28 @@
 <?php
   include "top.php";
-  $query = 'SELECT pmkPlanId, fldDateCreated, fldCatalogYear, fnkStudentId, fnkAdvisorId, pmkYear, pmkTerm, fnkCourseId 
+  $query = 'SELECT pmkPlanId, pmkYear, pmkTerm, fnkCourseId, fldDepartment, fldCourseNumber, fldCredits
 FROM tblPlan 
 INNER JOIN tblSemesterPlan on pmkPlanId = tblSemesterPlan.fnkPlanId
 INNER JOIN tblSemesterPlanCourses on tblSemesterPlan.fnkPlanId = tblSemesterPlanCourses.fnkPlanId 
 AND pmkYear = fnkYear 
 AND pmkTerm = fnkTerm
+INNER JOIN tblCourses on fnkCourseId = pmkCourseId
 WHERE pmkPlanId = 1
 ORDER BY tblSemesterPlan.fldDisplayOrder ASC, tblSemesterPlanCourses.fldDisplayOrder ASC';
-  $info2 = $thisDatabaseReader->select($query, "", 0,0,0,0, false, false);
-  echo count($info2);
+
+  #$thisDatabaseReader->testQuery($query, "", 0,0,0,0, false, false);
+  $records = $thisDatabaseReader->select($query, "", 0,0,0,0, false, false);
+
+
+  echo count($records);
   print '<br>';
   echo $query;
+
+
   print '<table>';
-  $columns = 8;
+  $columns = 7 ;
   $highlight = 0;
-  foreach ($info2 as $rec) {
+  foreach ($records as $rec) {
       $highlight++;
       if ($highlight % 2 != 0) {
           $style = ' odd ';
@@ -29,5 +36,42 @@ ORDER BY tblSemesterPlan.fldDisplayOrder ASC, tblSemesterPlanCourses.fldDisplayO
       print '</tr>';
   }
   print '</table>';
-include "footer.php";
+
+  $semesterCredits = 0;
+  $totalCredits = 0;
+
+  if(is_array($records)) {
+    foreach ($records as $row) {
+      if ($semester != $row['pmkTerm'] . $row['pmkYear']) {
+        if ($semester != '') {
+          print '</ol>';
+          print '<p>Total Credits: ' . $semesterCredits . '</p>';
+          print '</section>';
+        }
+        if ($semester != '' AND ($row['pmkTerm'] == 'FALL')) {
+          echo '</div>' . LINE_BREAK;
+        }
+        if ($row['pmkTerm'] == 'FALL') {
+          print '<div class="academicYear clearFloats">';
+        }
+        print '<section class="fourColumns';
+        print $row["pmkTerm"];
+
+        print '">';
+        print '<h3>' . $row['pmkTerm'] . ' ' . $row['pmkYear'] . '</h3>';
+        $semester = $row['pmkTerm'] . $row['pmkYear'];
+        $year = $row['pmkYear'];
+        $semesterCredits = 0;
+
+        print '<ol>';
+      }
+      print '<li class="' . $row['fldRequirement'] . '">';
+      print $row['fldDepartment'] . ' ' . $row['fldCourseNumber'];
+      print '</li>' . LINE_BREAK;
+      $semesterCredits = $semesterCredits + $row['fldCredits'];
+    }
+    print '<p>Total Credits: ' . $semesterCredits . '</p>';
+  }
+
+  include "footer.php";
 ?>
