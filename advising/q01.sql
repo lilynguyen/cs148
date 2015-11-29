993b25d765baee099c47b34744860b075739d44a
Lily Nguyen <lilynguyen@LHNMBPR.local>
2015-11-29 14:58:41 -0500

    folder, advising q01?

--- advising/q01.sql
+++ advising/q01.sql
@@ -6,3 +6,12 @@ AND pmkYear = fnkYear
 AND pmkTerm = fnkTerm
 WHERE pmkPlanId = 1
 ORDER BY tblSemesterPlan.fldDisplayOrder ASC, tblSemesterPlanCourses.fldDisplayOrder ASC;
+
+SELECT pmkPlanId, pmkYear, pmkTerm, fnkCourseId
+FROM tblPlan 
+INNER JOIN tblSemesterPlan on pmkPlanId = tblSemesterPlan.fnkPlanId
+INNER JOIN tblSemesterPlanCourses on tblSemesterPlan.fnkPlanId = tblSemesterPlanCourses.fnkPlanId 
+AND pmkYear = fnkYear 
+AND pmkTerm = fnkTerm
+WHERE pmkPlanId = 1
+ORDER BY tblSemesterPlan.fldDisplayOrder ASC, tblSemesterPlanCourses.fldDisplayOrder ASC;
---
+++ assignment10/header.php
@@ -0,0 +1,8 @@
+<!-- %%%%%%%%%%%%%%%%%%%%%%     Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
+
+<header>
+    <h1>ADVISING</h1>
+    <h2>things for advising and things</h2>
+</header>
+
+<!-- %%%%%%%%%%%%%%%%%%%%% Ends Page header   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% -->
