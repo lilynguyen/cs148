2f58c490d72c31c73f3b5007bd4adcf92e32ea57
Lily Hoai Nguyen <lhnguyen@silk3.uvm.edu>
2015-11-29 17:30:08 -0500

    css stuff

--- assignment10/footer.php
+++ assignment10/footer.php
@@ -1,8 +1,8 @@
 
 <!-- ===== START FOOTER ===== -->
     <footer>
-    	<img src='images/signature.png' alt='Signature Image'>
     	<p>Designed by <a href='http://www.uvm.edu/~lhnguyen'>Lily H. Nguyen</a></p>
+    	<img src='images/signature.png' alt='Signature Image' width='100px' height='auto'>
     </footer>
 <!-- ===== END FOOTER ===== -->
   </body>
