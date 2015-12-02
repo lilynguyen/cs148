da1140fe337e63240f112a7efd1f413f889d000f
Lily Nguyen <lilynguyen@ip0af517fc.int.uvm.edu>
2015-12-02 12:51:07 -0500

    bootstap

--- assignment10/top.php
+++ assignment10/top.php
@@ -15,6 +15,12 @@ require_once('lib/custom-functions.php');
     <script src="//html5shim.googlecode.com/sin/trunk/html5.js"></script>
     <![endif]-->
 
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    
     <link rel="stylesheet" href="css/base.css" type="text/css" media="screen">
 
     <?php
